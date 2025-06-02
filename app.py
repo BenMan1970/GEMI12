d0fb2d5ec989179fa0f816cbabd0673fcc007047
BenMan1970 <androidtunis@gmail.com>
2025-06-02 23:52:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+from datetime import datetime, timezone
+import requests
+
 # --- FETCH DATA ---
 @st.cache_data(ttl=900)
 def get_data(symbol):
